cf14c0412d02983a39fb2ed029085144235ae4dc
nsedarr@gmail.com <nospace1@users.noreply.github.com>
2016-08-02 08:54:58 -0400

    Added limitations to swarm

--- abilities/createswarm.py
+++ abilities/createswarm.py
@@ -1,9 +1,4 @@
 
-import bots
-#import connection
-#import abilities
-
-
 def noExistingSwarm(origin):
     #CHECK EVERYWHERAJLEJ
     for less in origin.lessers:
@@ -33,28 +28,34 @@ def createswarm(dictRef):
         if(len(data) < 3):
             size = 10
         else:
-            size = int(data[2])
-        if(len(data) < 4):
-            swarmlingNames = 'imp'
-        else:
-            swarmlingNames = data[3]
-        if(len(swarmlingNames) < 6):
+            if(data[2].isdigit()):
+                size = int(data[2])
+                if size < 31:
+                    if(len(data) < 4):
+                        swarmlingNames = 'imp'
+                    else:
+                        swarmlingNames = data[3]
+                    if(len(swarmlingNames) < 7):
 
-            #first createimp called swarm
-            #dictRef['data'] should equal 'botname createimp swarm'
-            swarmRef = dictRef
-            swarmRef['data'] = '{n} createimp {s}'.format(n=dictRef['bot'].name, s='swarm')
-            origin.performAbility('createimp', swarmRef)
-            #then createimp x times with swarm
-            swarmlingRef = swarmRef
-            swarmlingRef['bot'] = findSwarmBot(origin)
-            for x in xrange(size):
-                swarmlingRef['data'] = '{n} createimp {s}'.format(n='swarm', s=swarmlingNames+str(x+1))
-                swarmlingRef['bot'].performAbility('createimp', swarmlingRef)
-            #then set variable
-            swarmlingRef['bot'].isSwarm = True
-        else:
-            dictRef['bot'].talk(dictRef['where'], "Too large of swarmling name, needs to be 5 or less ")
+                        #first createimp called swarm
+                        swarmRef = dictRef
+                        swarmRef['data'] = '{n} createimp {s}'.format(n=dictRef['bot'].name, s='swarm')
+                        origin.performAbility('createimp', swarmRef)
+                        #then createimp x times with swarm
+                        swarmlingRef = swarmRef
+                        swarmlingRef['bot'] = findSwarmBot(origin)
+                        for x in xrange(size):
+                            swarmlingRef['data'] = '{n} createimp {s}'.format(n='swarm', s=swarmlingNames+str(x+1))
+                            swarmlingRef['bot'].performAbility('createimp', swarmlingRef)
+
+                        #then set variable
+                        swarmlingRef['bot'].isSwarm = True
+                    else:
+                        dictRef['bot'].talk(dictRef['where'], "Too large of swarmling name, needs to be 6 or less ")
+                else:
+                    dictRef['bot'].talk(dictRef['where'], "Must be of size 30 or less")
+            else:
+                dictRef['bot'].talk(dictRef['where'], "{d} is not a number".format(d=data[2]))
     else:
         dictRef['bot'].talk(dictRef['where'], "There already exists a swarm")
 
